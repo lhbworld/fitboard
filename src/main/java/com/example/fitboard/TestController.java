9a111a9cacc261a2f4ee7b88ae020ffc1cebc688
이현빈 <lhbworld@naver.com>
2026-04-09 17:44:15 +0900

    프로젝트 초기 세팅 및 테스트 API 구성

---
+++ src/main/java/com/example/fitboard/TestController.java
@@ -0,0 +1,13 @@
+package com.example.fitboard;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "서버 정상 작동";
+    }
+}
---
+++ src/test/java/com/example/fitboard/FitboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
