f6e9d05471fbd9adea1af9b3e97018bface16c7f
이현빈 <lhbworld@naver.com>
2026-05-03 23:39:19 +0900

    옮기는 과정 누락됐었던 user 엔티티

--- src/main/java/com/example/fitboard/user/entity/User.java
+++ src/main/java/com/example/fitboard/user/entity/User.java
@@ -1,4 +1,35 @@
 package com.example.fitboard.user.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String loginId;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 30)
+    private String nickname;
+
+    public User(String loginId, String password, String email, String nickname) {
+        this.loginId = loginId;
+        this.password = password;
+        this.email = email;
+        this.nickname = nickname;
+    }
 }
